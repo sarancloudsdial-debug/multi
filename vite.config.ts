0c7c08aa1e647c484862405cb018bc9962363ded
saran8008 <saranthiyagu10@gmail.com>
2025-09-26 12:25:13 +0530

    Update package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
- 
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: "/multipi/",   // 👈 repo name only, not full URL
-  server: {
-    host: true,
-    strictPort: true,
-    port: 5173,
-    allowedHosts: [
-      "myreactapp.loca.lt",
-      ".loca.lt",
-    ],
-  },
-});
- 
- 
+  base: '/multipi/',   // 👈 important
+})
