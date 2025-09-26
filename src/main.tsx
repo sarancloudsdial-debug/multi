08c39fc2707bb7b4163950ec716d901deab3ba34
saran8008 <saranthiyagu10@gmail.com>
2025-09-26 12:27:02 +0530

    Update main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = document.getElementById("root") as HTMLElement;
 const base = import.meta.env.BASE_URL || "/";
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
+  
  
-  <HashRouter>
+  <BrowserRouter>
     <App />
-</HashRouter>
-  </React.StrictMode>
+</BrowserRouter>
+
 );
