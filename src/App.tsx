69649f5382096250e771f4f87cc5b690b9be0948
saran8008 <saranthiyagu10@gmail.com>
2025-09-26 15:22:03 +0530

    Describe what you changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,79 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./component/ScrollToTop"; // ✅ import  
+import ScrollToTop from "./component/ScrollToTop";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+
+// Pages
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import DemoPage from "./pages/Demo";
 import FreeTrail from "./pages/FreeTrail";
 import Login from "./pages/login";
+import MultipiCrm from "./pages/MultipiCrm";
+
+// Resources
 import HelpCenter from "./Menu/Resources/HelpCenter";
 import Blogs from "./Menu/Resources/Blogs";
+
+// Solutions
 import Growth from "./Menu/SolutionsMenu/Growth";
-// import MultipiCrm from "./Menu/SolutionsMenu/MultipiAssist";
-import MultipiAssist from "./Menu/SolutionsMenu/MultipiAssist"
-import OnlineEducation from "./Menu/SolutionsMenu/OnlineEducation"
-import MultipiCrm from "./pages/MultipiCrm";
+import MultipiAssist from "./Menu/SolutionsMenu/MultipiAssist";
+import OnlineEducation from "./Menu/SolutionsMenu/OnlineEducation";
 import DigitalCommerce from "./Menu/SolutionsMenu/DigitalCommerce";
-import BrandingAgencies from "./Menu/SolutionsMenu/BrandingAgencies"
+import BrandingAgencies from "./Menu/SolutionsMenu/BrandingAgencies";
 import MedicalService from "./Menu/SolutionsMenu/MedicalService";
+
+// Products
 import AiSupport from "./Menu/Product/Ai-support";
-import ChatBotAutomation from"./Menu/Product/ChatbotAutomation";
-import InstagramFaceBook from"./Menu/Product/InstagramFaceBook";
+import ChatBotAutomation from "./Menu/Product/ChatbotAutomation";
+import InstagramFaceBook from "./Menu/Product/InstagramFaceBook";
 import BroadCast from "./Menu/Product/BroadCast";
 import ClicktoWhatsapp from "./Menu/Product/Clicktowhatsapp";
 import Teaminbox from "./Menu/Product/Teaminbox";
- 
- 
+
 const App = () => {
   return (
     <Router>
       <Header />
-        <ScrollToTop />   {/* ✅ Added */}
+      <ScrollToTop />
+
       <Routes>
-        <Route path="/multi" element={<Home />} />        
+        {/* Default Home Page */}
+        <Route path="/" element={<Home />} />
+        <Route path="/multi" element={<Home />} />
+
+        {/* Pages */}
         <Route path="/demo" element={<DemoPage />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/freeTrail" element={<FreeTrail />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/help" element={<HelpCenter/>}/>
-        <Route path="/blog" element={<Blogs/>}/>
-        <Route path="/Multipigrowth" element={<Growth />} /> {/* Growth page */}
-        <Route path="/MultipiCrm" element={<MultipiCrm />} /> {/* Growth page */}
-        <Route path="/Multipiassist" element={<MultipiAssist/>} />
-        <Route path="/OnlineEducation" element={<OnlineEducation/>}/>
-        <Route path="/DigitalCommerce" element={<DigitalCommerce/>}/>
-        <Route path="/brandingAgencies" element={<BrandingAgencies/>}/>
-        <Route path="/MedicalService" element={<MedicalService/>}/>
-        <Route path="/Aisupport" element={<AiSupport/>}/>
-        <Route path="/Chatbot" element={<ChatBotAutomation/>}/>
-        <Route path="/InstgramFaceBook" element={<InstagramFaceBook/>}/>
-        <Route path="/BroadCast" element={<BroadCast/>}/>
-        <Route path="/ClicktoWhatsapp"element={<ClicktoWhatsapp/>}/>
-        <Route path="/Teaminbox" element={<Teaminbox/>}/>
-       
+        <Route path="/free-trial" element={<FreeTrail />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Resources */}
+        <Route path="/help" element={<HelpCenter />} />
+        <Route path="/blog" element={<Blogs />} />
+
+        {/* Solutions */}
+        <Route path="/multipi-growth" element={<Growth />} />
+        <Route path="/multipi-crm" element={<MultipiCrm />} />
+        <Route path="/multipi-assist" element={<MultipiAssist />} />
+        <Route path="/online-education" element={<OnlineEducation />} />
+        <Route path="/digital-commerce" element={<DigitalCommerce />} />
+        <Route path="/branding-agencies" element={<BrandingAgencies />} />
+        <Route path="/medical-service" element={<MedicalService />} />
+
+        {/* Products */}
+        <Route path="/ai-support" element={<AiSupport />} />
+        <Route path="/chatbot" element={<ChatBotAutomation />} />
+        <Route path="/instagram-facebook" element={<InstagramFaceBook />} />
+        <Route path="/broadcast" element={<BroadCast />} />
+        <Route path="/click-to-whatsapp" element={<ClicktoWhatsapp />} />
+        <Route path="/team-inbox" element={<Teaminbox />} />
       </Routes>
+
       <Footer />
     </Router>
   );
 };
- 
+
 export default App;
- 
- 
- 
