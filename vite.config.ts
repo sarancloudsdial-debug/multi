985c690656f5a04168af26617c13cccd1a96275a
saran8008 <saranthiyagu10@gmail.com>
2025-09-26 10:44:24 +0530

    update vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+ 
 export default defineConfig({
   plugins: [react()],
-  base: "/multi/", // <-- for GitHub Pages
+  base: "/my-react-app/",   // 👈 repo name only, not full URL
   server: {
     host: true,
     strictPort: true,
     port: 5173,
     allowedHosts: [
-      "myreactapp.loca.lt", // your custom subdomain
-      ".loca.lt",           // allow any loca.lt subdomain
+      "myreactapp.loca.lt",
+      ".loca.lt",
     ],
   },
 });
+ 
+ 
