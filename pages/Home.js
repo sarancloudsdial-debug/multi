599133a37620dd623360193b5314d2a3c1256b50
saran8008 <saranthiyagu10@gmail.com>
2025-10-04 10:42:57 +0530

    Updates

--- pages/Home.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Hero from "../component/Hero";
-import Features from "../component/Features";
-import Testimonials from "../component/Testimonials";
-// import BlogPage from '../component/BlogPage';
-function Home() {
-    return (_jsxs("div", { children: [_jsx(Hero, {}), _jsx(Features, {}), _jsx(Testimonials, {})] }));
-}
-export default Home;
